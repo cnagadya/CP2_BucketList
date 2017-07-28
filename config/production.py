62e9799941996d8e24319c8545f6da7e52080c07
cnagadya <ncritah@gmail.com>
2017-07-28 21:39:35 +0300

    Setting the configuration files

--- config/production.py
+++ config/production.py
@@ -1,8 +1,12 @@
-import os, random, string
+import os
+import random
+import string
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, '../database/bucketlist.db')
 
 DEBUG = False
-SECRET_KEY = "".join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+db_path
+SECRET_KEY = "".join(random.choice(
+    string.ascii_uppercase + string.digits) for x in range(32))
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'DATABASE_URL') or 'sqlite:///' + db_path
--- config/testing.py
+++ config/testing.py
@@ -1,9 +1,12 @@
-import os, random, string
+import os
+import random
+import string
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, '../database/test.db')
 
 DEBUG = False
 TESTING = True
-SECRET_KEY = "".join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+db_path
+SECRET_KEY = "".join(random.choice(
+    string.ascii_uppercase + string.digits) for x in range(32))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path

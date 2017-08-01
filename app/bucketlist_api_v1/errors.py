b34a090ef3715516a9bed31b37134b8f20726179
cnagadya <ncritah@gmail.com>
2017-08-01 10:03:58 +0300

    Adding customized errors

--- app/bucketlist_api_v1/errors.py
+++ app/bucketlist_api_v1/errors.py
@@ -2,29 +2,26 @@
 from flask import jsonify
 from . import blist_api
 
-class ValidationError(ValueError):
-    pass
-
-
-@blist_api.errorhandler(ValidationError)
+@blist_api.app_errorhandler(400)
 def bad_request(e):
-    return jsonify({'message': e.args[0]}), 400
+    return jsonify({'message': "Missing Required Paremeters"}), 400
 
 
-@blist_api.errorhandler(404)
+
+@blist_api.app_errorhandler(404)
 def not_found(e):
     return jsonify({'message': 'Invalid resource URI',"Status Code":"404"}), 404
 
-@blist_api.errorhandler(401)
+@blist_api.app_errorhandler(401)
 def unauthorized_access(e):
     return jsonify({'message': 'Unauthorized Access! You need an authorization token to access this page'}), 401
 
 
-@blist_api.errorhandler(405)
+@blist_api.app_errorhandler(405)
 def method_not_supported(e):
     return jsonify({'message': 'Method is not supported'}), 405
 
 
 @blist_api.app_errorhandler(500)
 def internal_server_error(e):
-    return jsonify({'message': e.args[0]}), 500
+    return jsonify({'message': "Internal server error"}), 500

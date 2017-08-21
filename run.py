d1378ea8682083966c1accc721778e39ff3aed77
cnagadya <ncritah@gmail.com>
2017-08-21 15:02:16 +0300

    A
    run.py changes for heroku hosting

--- run.py
+++ run.py
@@ -2,12 +2,13 @@
 import os
 
 from app import create_app
-from app.models import User
+
+
+app,db = create_app(os.environ.get('FLASK_CONFIG','development'))
+    #if no config set, use development
+with app.app_context():
+     db.create_all()
+
 
 if __name__ == '__main__':
-    app,db = create_app(os.environ.get('FLASK_CONFIG','development'))
-    #if no config set, use development
-    with app.app_context():
-        db.create_all()
-
     app.run(port=8080)
